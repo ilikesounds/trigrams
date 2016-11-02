037cfde559740e2b0fc32da13808beac704486db
ilikesounds <jeffrey.n.torres@gmail.com>
2016-11-01 22:53:00 -0700

    created open document function

--- trigrams.py
+++ trigrams.py
@@ -1,29 +1,12 @@
 # -*- encode: utf-8 -*-
 import io
-from string import maketrans
 
-book = io.open('./data/Uncle_Sams.txt', mode='r', encoding='utf-8')
-book_text = book.read(12950)
-book.close()
-usable_text = book_text[2951:]
 
-inrep  = u',.?!"-'
-outrep = u'      '
-
-tranrep = maketrans(inrep, outrep)
-new_text = usable_text.translate(tranrep)
-
-print(new_text)
-
-# usable_text = '"We must break this up," whispered Private Hyman to Noll Terry, Hal Overton\'s soldier chum. "I don\'t want to see him get hurt."'
-# new_text = usable_text.replace(',', '').replace('"', '')\
-    # .replace('.', '').replace('?', '').replace('!', '').replace('-', '').replace('\n', ' ')
-
-# print(new_text)
-
-# split_text = new_text.split(' ', 2)
-# print(usable_text[0:100])
-
-# dict_key = split_text[0] + ' ' + split_text[1]
-
-# print(dict_key)
+def create_raw_string(book):
+    """
+    Get text from book and transform into raw string
+    """
+    open_file = io.open(book, mode='r', encoding='utf-8')
+    raw_string = open_file.read(13000)
+    open_file.close()
+    return raw_string[2727:]
