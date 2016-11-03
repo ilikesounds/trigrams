2baa8701cae3b35b57943794f262f054eea2c7cc
ilikesounds <jeffrey.n.torres@gmail.com>
2016-11-02 20:08:43 -0700

    created second function test

--- src/test_trigram.py
+++ src/test_trigram.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from trigrams import create_raw_string
+import pytest
+from trigrams import(
+    create_raw_string,
+    format_string_for_list,
+    create_trigram_dict
+    )
 
 '''
 Test file for trigrams assignment
 '''
 
-TEXT = """The young recruit's mistake lay in pretending to know a lot more than
+TEXT = '''The young recruit's mistake lay in pretending to know a lot more than
 he really did know. He had been put through the unmerciful hazing that
 always awaits a very "fresh" rookie, or recruit, but even that had
 taught him little. Private Green was always looking for the chance to
@@ -14,7 +19,17 @@ prove to his new comrades among the regulars of the Thirty-fourth that
 he knew something after all. This afternoon his trouble had taken the
 form of trying to find something in a two-days' old newspaper on which
 he could discourse for the enlightenment of the other men.
-"""
+'''
+
+PUNCTUATION = [
+    ',',
+    '.',
+    '?',
+    '!',
+    '"',
+    '-',
+    '\n'
+]
 
 
 def test_create_rawstring():
@@ -22,3 +37,12 @@ def test_create_rawstring():
     Test create_raw_string
     '''
     assert TEXT in create_raw_string('./data/Uncle_Sams.txt')
+
+
+@pytest.mark.parametrize('punc', PUNCTUATION)
+def test_format_string_for_list_punctuation(punc):
+    '''
+    Test format_string_for_list to ensure all punctuation and newlines are
+    removed.
+    '''
+    assert punc not in format_string_for_list(TEXT)
